9cb01a7e3d28373154fa7d441a0147e0affc21e5
Nofar Yunger <nofar505@gmail.com>
2022-01-24 19:28:54 +0200

    deleting all the useless files

--- services/db.service.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require("sequelize/dist");
-
-const database = process.env.MYSQL_DATABASE;
-const password = process.env.MYSQL_PASSWORD;
-
-
-var sequelize = new Sequelize(database, username, [password]);
-
